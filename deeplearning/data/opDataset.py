d64f0c93faa3fc29bf5b64bc53b1b71fcb2d131d
Markus Döring <webmaster@burgerdev.de>
2015-09-10 10:24:02 +0200

    added target handlign to OpDataset

--- deeplearning/data/opDataset.py
+++ deeplearning/data/opDataset.py
@@ -1,4 +1,6 @@
 
+from functools import wraps
+
 import numpy as np
 
 from lazyflow.operator import Operator, InputSlot, OutputSlot
@@ -7,6 +9,20 @@ from lazyflow.rtype import SubRegion
 from pylearn2.datasets import Dataset
 
 
+def _assert_input_ready(method):
+    """
+    wrapper for OpDataset methods to prevent usage before input is ready
+    """
+    @wraps(method)
+    def wrapped(self, *args, **kwargs):
+        if len(self.Input) != 2:
+            raise RuntimeError("input slot needs data and target")
+        if not self.Input[0].ready() or not self.Input[1].ready():
+            raise RuntimeError("input is not ready, "
+                               "can't use dataset yet")
+        return method(self, *args, **kwargs)
+    return wrapped
+
 class OpDataset(Operator, Dataset):
     """
     converts an input slot to a pylearn2 dataset
@@ -14,30 +30,33 @@ class OpDataset(Operator, Dataset):
     the Output slot simply replicates the input slot, accessing data is done
     via the pylearn2.dataset.Dataset interface
     """
-    Input = InputSlot()
-    Output = OutputSlot()
+    Input = InputSlot(level=1)
+    Output = OutputSlot(level=1)
+
+    def __init__(self, *args, **kwargs):
+        super(OpDataset, self).__init__(*args, **kwargs)
+        self.Output.connect(self.Input)
 
     def setupOutputs(self):
-        self.Output.meta.assignFrom(self.Input.meta)
-        self._num_examples = self.Input.meta.shape[0]
-        self._example_shape = tuple(self.Input.meta.shape)[1:]
+        self._num_examples = self.Input[0].meta.shape[0]
+        assert self._num_examples == self.Input[1].meta.shape[0]
 
     def propagateDirty(self, slot, subindex, roi):
         self.Output.setDirty(roi)
 
     def execute(self, slot, subindex, roi, result):
-        req = self.Input.get(roi)
-        req.writeInto(result)
-        req.block()
+        raise RuntimeError("should not reach this method")
 
     # METHODS FOR DATASET
 
     def has_targets(self):
-        return False
+        return True
 
+    @_assert_input_ready
     def get_num_examples(self):
         return self._num_examples
 
+    @_assert_input_ready
     def iterator(self, mode=None, batch_size=None, num_batches=None, rng=None,
                  data_specs=None, return_tuple=False):
         if mode is None:
@@ -47,7 +66,6 @@ class OpDataset(Operator, Dataset):
         assert rng is None, "rng handling not implemented"
 
         n = int(self._num_examples)
-        s = self._example_shape
 
         if batch_size is None:
             if num_batches is None:
@@ -58,6 +76,15 @@ class OpDataset(Operator, Dataset):
         else:
             num_batches = int(np.ceil(float(n)/batch_size))
 
+        # default data returned is 'features'
+        data_type = 0
+        if data_specs is not None:
+            assert len(data_specs) == 2
+            if data_specs[1] == "target":
+                data_type = 1
+
+        s = tuple(self.Input[data_type].meta.shape)[1:]
+
         for b in range(num_batches):
             left = b*batch_size
             right = (b+1)*batch_size
@@ -66,7 +93,7 @@ class OpDataset(Operator, Dataset):
             start = (left,) + (0,)*len(s)
             stop = (right,) + s
             new_roi = SubRegion(self.Input, start=start, stop=stop)
-            X = self.Input.get(new_roi).wait()
+            X = self.Input[data_type].get(new_roi).wait()
 
             if return_tuple:
                 yield (X,)
--- test/testOpDataset.py
+++ test/testOpDataset.py
@@ -14,16 +14,23 @@ class TestOpDataset(unittest.TestCase):
     def setUp(self):
         X = np.random.random(size=(1000, 5))
         X = vigra.taggedView(X, axistags='tc')
+        y = np.random.randint(0, 2, size=(1000,))
+        y = vigra.taggedView(y, axistags='t')
         self.X = X
+        self.y = y
 
         self.g = Graph()
 
-        self.pipe = OpArrayPiperWithAccessCount(graph=self.g)
+        self.pipeData = OpArrayPiperWithAccessCount(graph=self.g)
+        self.pipeTarget = OpArrayPiperWithAccessCount(graph=self.g)
 
         self.op = OpDataset(graph=self.g)
-        self.op.Input.connect(self.pipe.Output)
+        self.op.Input.resize(2)
+        self.op.Input[0].connect(self.pipeData.Output)
+        self.op.Input[1].connect(self.pipeTarget.Output)
 
-        self.pipe.Input.setValue(self.X)
+        self.pipeData.Input.setValue(self.X)
+        self.pipeTarget.Input.setValue(self.y)
 
     def test_adjust_for_viewer(self):
         self.op.adjust_for_viewer(self.X)
@@ -33,7 +40,7 @@ class TestOpDataset(unittest.TestCase):
         np.testing.assert_equal(n, self.X.shape[0])
 
     def test_has_targets(self):
-        assert not self.op.has_targets()
+        assert self.op.has_targets()
 
     def test_iterator(self):
         k = 5
