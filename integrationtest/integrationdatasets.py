2775ed13a7a12c1c2dbc4bd8d60afa72d8ef308c
Markus Döring <webmaster@burgerdev.de>
2015-09-28 10:16:40 +0200

    refactored datasets and tests

---
+++ integrationtest/integrationdatasets.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+import vigra
+
+from lazyflow.utility.testing import OpArrayPiperWithAccessCount
+
+TAU = 2*np.pi
+
+
+class _BaseDataset(OpArrayPiperWithAccessCount):
+    @classmethod
+    def build(cls, d, graph=None, parent=None, workingdir=None):
+        np.random.seed(hash(cls.__name__))
+        data = cls.createDataset(d)
+        op = cls(parent=parent, graph=graph)
+        op.Input.setValue(data)
+        return op
+
+    @classmethod
+    def createDataset(cls, config):
+        raise NotImplementedError()
+
+
+class OpNoisySine(_BaseDataset):
+    @classmethod
+    def createDataset(cls, config):
+        assert "shape" in config
+        num_examples = config["shape"][0]
+        num_periods = 99.9
+        data = np.linspace(0, num_periods*TAU, num_examples)
+        data = (np.sin(data) + 1) / 2
+        noise = np.random.normal(loc=0, scale=.02, size=(num_examples,))
+        data += noise
+        data = vigra.taggedView(data, axistags="t")
+        return data
--- integrationtest/testMLPRegression.py
+++ integrationtest/testMLPRegression.py
@@ -1,14 +1,9 @@
 
 import warnings
-import unittest
-import shutil
 import tempfile
 
 import numpy as np
 import vigra
-
-from lazyflow.utility.testing import OpArrayPiperWithAccessCount
-from lazyflow.operators import OpReorderAxes
 
 from deeplearning.workflow import Workflow
 from deeplearning.split import OpTrainTestSplit
@@ -22,28 +17,11 @@ from deeplearning.targets import OpExponentiallySegmentedPattern
 
 from pylearn2.models import mlp
 
-TAU = 2*np.pi
-
-
-class OpSource(OpArrayPiperWithAccessCount):
-    @staticmethod
-    def build(d, graph=None, parent=None, workingdir=None):
-        assert "shape" in d
-        num_examples = d["shape"][0]
-        num_periods = 99.9
-        data = np.linspace(0, num_periods*TAU, num_examples)
-        data = (np.sin(data) + 1) / 2
-        np.random.seed(1)
-        noise = np.random.normal(loc=0, scale=.02, size=(num_examples,))
-        data += noise
-        data = vigra.taggedView(data, axistags="t")
-        op = OpSource(parent=parent, graph=graph)
-        op.Input.setValue(data)
-        return op
+from integrationdatasets import OpNoisySine
 
 
 config = {"class": Workflow,
-          "source": {"class": OpSource,
+          "source": {"class": OpNoisySine,
                      "shape": (10000,)},
           "features": {"class": OpRecent, "window_size": 30},
           "target": {"class": OpExponentiallySegmentedPattern,
@@ -60,30 +38,26 @@ config = {"class": Workflow,
 
 class TestMLPRegression(object):
     def setUp(self):
-        pass
+        self.wd = tempfile.mkdtemp(prefix="MLPReg_")
+
+    def tearDown(self):
+        import sys
+        sys.stderr.write("testMLP: {}\n".format(self.wd))
+        # TODO remove dir
+        # shutil.rmtree(d)
+        
 
     def testRun(self):
-        d = tempfile.mkdtemp(prefix="MLPReg_")
-        try:
-            c = config.copy()
-            c["train"] = {"class": OpMLPTrain,
-                          "layer_classes": (mlp.Sigmoid, mlp.Sigmoid),
-                          "layer_sizes": (20, 10)}
-            c["predict"] = {"class": OpMLPPredict}
-            w = Workflow.build(c, workingdir=d)
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore")
-                w.run()
-            self.__verify(w)
-        except:
-            w = None
-            raise
-        finally:
-            import sys
-            sys.stderr.write("testMLP: {}\n".format(d))
-            # TODO remove dir
-            # shutil.rmtree(d)
-            return w
+        c = config.copy()
+        c["train"] = {"class": OpMLPTrain,
+                      "layer_classes": (mlp.Sigmoid, mlp.Sigmoid),
+                      "layer_sizes": (20, 10)}
+        c["predict"] = {"class": OpMLPPredict}
+        w = Workflow.build(c, workingdir=self.wd)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            w.run()
+        self.__verify(w)
 
     def __verify(self, w):
         pass
@@ -93,7 +67,10 @@ if __name__ == "__main__":
     from matplotlib import pyplot as plt
     test = TestMLPRegression()
     test.setUp()
-    w = test.testRun()
+    try:
+        w = test.testRun()
+    finally:
+        test.tearDown()
 
     pred = w._predictionCache.Output[...].wait()
     target = w._target.Output[...].wait()
