2898c0f6613623518cd0f5f31c5811242265e269
Markus Döring <webmaster@burgerdev.de>
2015-09-30 10:48:54 +0200

    actually added batch processing

---
+++ test/testBatch.py
@@ -0,0 +1,40 @@
+
+import os
+import unittest
+import tempfile
+import shutil
+
+from deeplearning.batch import runBatch
+
+from deeplearning.classifiers import OpSVMTrain
+from deeplearning.classifiers import OpSVMPredict
+
+from deeplearning.data.integrationdatasets import OpShuffledLinspace
+from deeplearning.data.integrationdatasets import OpTarget
+from deeplearning.data.integrationdatasets import OpFeatures
+
+from deeplearning.data.wrappers import OpArrayPiper
+
+from deeplearning.report import OpClassificationReport
+from deeplearning.report import OpRegressionReport
+
+
+class TestMLPRegression(unittest.TestCase):
+    def setUp(self):
+        self.wd = tempfile.mkdtemp(prefix="BatchRun_")
+
+    def testBatch(self):
+        os.mkdir(os.path.join(self.wd, "1"))
+        config = {"source": {"class": OpShuffledLinspace,
+                             "shape": (1000,)},
+                  "preprocessing": ({"class": OpArrayPiper},),
+                  "features": {"class": OpFeatures},
+                  "target": {"class": OpTarget},
+                  "train": {"class": OpSVMTrain},
+                  "predict": {"class": OpSVMPredict},
+                  "report": {"class": [OpClassificationReport,
+                                       OpRegressionReport]}}
+        runBatch(config, self.wd)
+
+    def tearDown(self):
+        shutil.rmtree(self.wd)
